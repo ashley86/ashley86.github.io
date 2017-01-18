7d32b58972de5d79bb9834dbf33d03649b3d917f
Ashley <ashley.ramtohul@gmail.com>
2017-01-18 10:36:11 +0100

    exercice poo

---
+++ wf3/cours/exercices/php/poo/18-01/9-Vehicule.php
@@ -0,0 +1,58 @@
+<?php
+
+abstract class Vehicule
+{
+    final public function demarrer()
+    {
+        return 'je démarre';
+    }
+
+    public function getVitesseMax()
+    {
+        return 100;
+    }
+
+    abstract public function getCarburant();
+
+}
+
+class Moto extends Vehicule
+{
+    public function getCarburant()
+    {
+        return 'essence';
+    }
+
+    public function getVitesseMax()
+    {
+        $parentVitesseMax = parent::getVitesseMax();
+        return $parentVitesseMax += 60;
+    }
+}
+
+class Voiture extends Vehicule
+{
+    public function getCarburant()
+    {
+        return 'diesel';
+    }
+
+    public function getVitesseMax()
+    {
+        $parentVitesseMax = parent::getVitesseMax();
+        return $parentVitesseMax += 30;
+    }
+}
+
+
+$voiture = new Voiture();
+echo 'Voiture démarre : ' . $voiture->demarrer() . '<br />';
+echo 'Voiture carburant : ' . $voiture->getCarburant() . '<br />';
+echo 'Voiture vitesse max : ' . $voiture->getVitesseMax() . '<br />';
+
+echo '<hr />';
+
+$moto = new Moto();
+echo 'Mote démarre : ' . $moto->demarrer() . '<br />';
+echo 'Mote carburant : ' . $moto->getCarburant() . '<br />';
+echo 'Mote vitesse max : ' . $moto->getVitesseMax() . '<br />';
